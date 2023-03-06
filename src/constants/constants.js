2c0b2a5c182400890831267fc5540da783679c09
vaajiee <vajidmohammed94@gmail.com>
2023-03-06 17:01:03 +0530

    Home page Ui completed

---
+++ src/constants/constants.js
@@ -0,0 +1,11 @@
+const key = '7a29ad8c90a44188fe2a8cfadf7fdebb'
+
+const requests = {
+    requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
+    requestTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=1`,
+    requestTrending: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=2`,
+    requestHorror: `https://api.themoviedb.org/3/search/movie?api_key=${key}&query=horror&language=en-US&page=1&include_adult=false`,
+    requestUpcoming: `https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page=1`
+}
+
+export default requests
